e3697caff82b1f32c79af390fdab1e41187b6c2a
sebastian.j.bogado <sebastian.j.bogado@gmail.com>
2011-10-27 16:29:51 +0000

    Todo armado para que funcione. Falta escribir los shaders en sí, pero ya están puestos Faltan también las luces.

--- TP2/TP2/ShaderManager.h
+++ TP2/TP2/ShaderManager.h
@@ -1,13 +1,66 @@
 #pragma once
 #include "GLSLProgram.h"
+#include "GLSLangShader.h"
+
+enum VertexShader {
+	RETORCER,
+	RUIDO,
+	DOBLAR,
+	ESFERIZAR
+};
+
+enum FragmentShader{
+	MATERIAL_SOMBREADO_BRILLANTE,
+	MATERIAL_SOMBREADO_TEXTURADO,
+	MATERIAL_REFLECTIVO,
+	MATERIAL_SOMBREADO_SEMIMATE
+};
+
 
 class ShaderManager{
 public:
+	//crea todos los shaders de la aplicación
 	ShaderManager(void);
 	~ShaderManager(void);
+	void setVertexShader(VertexShader tipo);
+	void setFragmenShader(FragmentShader tipo);
+	void usarPrograma();
+
+private:
+	GLuint vertexShaderSeleccionado;
+	GLuint fragmentShaderSeleccionado;
+
+	GLuint* vertexShaders;
+	GLuint* fragmentShaders;
+	
+	//estas variables controlan si hubo algún cambio entre el shader anterior y el actual, para ahorra procesamiento
+	bool cambioElFragmentShader;
+	bool cambioElVertexShader;
+
+	GLSLProgram programa;
+	
+	//ubicación de los archivos
+	static const string NOMBRE_ARCHIVO_VERTEX_SHADER_RETORCER;
+	static const string NOMBRE_ARCHIVO_VERTEX_SHADER_RUIDO;
+	static const string NOMBRE_ARCHIVO_VERTEX_SHADER_DOBLAR;
+	static const string NOMBRE_ARCHIVO_VERTEX_SHADER_ESFERIZAR;
+
+	static const string NOMBRE_ARCHIVO_FRAGMENT_SHADER_MATERIAL_SOMBREADO_BRILLANTE;
+	static const string NOMBRE_ARCHIVO_FRAGMENT_SHADER_MATERIAL_SOMBREADO_TEXTURADO;
+	static const string NOMBRE_ARCHIVO_FRAGMENT_SHADER_MATERIAL_REFLECTIVO;
+	static const string NOMBRE_ARCHIVO_FRAGMENT_SHADER_MATERIAL_SOMBREADO_SEMIMATE;
+
+	void inicializarVertexShaders();
+	void inicializarFragmentShaders();
+
 	void usarVertexShaderRetorcer();
 	void usarVertexShaderRuido();
 	void usarVertexShaderDoblar();
 	void usarVertexShaderEsferizar();
+
+	void usarFragmentShaderMaterialSombreadoBrillante();
+	void usarFragmentShaderMaterialSombreadoTexturado();
+	void usarFragmentShaderMaterialReflectivo();
+	void usarFragmentShaderMaterialSombreadoSemimate();
 };
 
