1941e40d64c7ba35dd017eae45b43117fe002d78
mat1204.torres <mat1204.torres@gmail.com>
2012-02-17 03:00:08 +0000

    Algunas cosas mas agregadas a la clase Control y arreglado al menos temporalmente el problema de la camara

--- TP Final/TP/Control.h
+++ TP Final/TP/Control.h
@@ -2,6 +2,8 @@
 
 #include "..\Camara\Camara.h"
 
+#include <GL\freeglut.h>
+
 class Control{
 public:
 	Control(bool debug);
@@ -10,10 +12,18 @@ public:
 	static void mouse(int button, int state, int x, int y);
 	static void mouseMov(int x, int y);
 
+	static void dibujarEscena();
+	
+	//Suplantaria a la funcion OnIdle
+	static void actualizarDatos();
+
 	static float atPorDefecto[3];
 	static float posicionPorDefecto[3];
 	static float upPorDefecto[3];
+
+	
 private:
 	static Camara* camara;
+	
 	bool debug;
 };
